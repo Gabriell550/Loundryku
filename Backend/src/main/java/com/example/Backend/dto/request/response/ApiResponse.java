0cf75068895983a27739f51cc0db7fc3807210ef
gabriell <gloxiniadrole890@gmail.com>
2026-07-11 14:56:52 +0700

    inisiasi file backend

---
+++ Backend/src/main/java/com/example/Backend/dto/request/response/ApiResponse.java
@@ -0,0 +1,5 @@
+package com.example.Backend.dto.request.response;
+
+public class ApiResponse {
+    
+}
---
+++ Backend/src/main/java/com/example/Backend/repository/UserRepository.java
@@ -0,0 +1,5 @@
+package com.example.Backend.repository;
+
+public class UserRepository {
+    
+}
