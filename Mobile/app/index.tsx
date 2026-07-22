dc22779ece69888f76369ac96b8e96d1acce520a
brilian <brilianfaiz734@gmail.com>
2026-07-22 15:18:43 +0800

    menambahkan tabel userManage menggunakan lib table react

--- Mobile/app/index.tsx
+++ Mobile/app/index.tsx
@@ -1,5 +1,5 @@
 import { Redirect } from 'expo-router';
 
 export default function Index() {
-  return <Redirect href={'/register' as any} />;
+  return <Redirect href={'/userManagement' as any} />;
 }
